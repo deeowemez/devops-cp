e370c8697b6720787805a1fe41cccdcd203036fd
deeowemez <53809101+deeowemez@users.noreply.github.com>
2026-05-31 21:10:48 +0800

    update tracing url

--- app/backend/src/tracing.js
+++ app/backend/src/tracing.js
@@ -4,7 +4,7 @@ const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http')
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
 
 const sdk = new NodeSDK({
-  traceExporter: new OTLPTraceExporter({ url: 'http://tempo:4318/v1/traces' }),
+  traceExporter: new OTLPTraceExporter({ url: 'http://tempo.observability.svc.cluster.local:4318/v1/traces' }),
   instrumentations: [getNodeAutoInstrumentations()],
   serviceName: 'task-app-backend'
 });
